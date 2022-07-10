316005659a7be46620ff759d0ac2c04831de35f1
Naveena <kalari-sai.naveena@capgemini.com>
2022-07-10 21:08:09 +0530

    first

---
+++ src/test/java/com/jenkins/SpringJenkinsApplicationTests.java
@@ -0,0 +1,25 @@
+package com.jenkins;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringJenkinsApplicationTests {
+
+	public static Logger logger= LoggerFactory.getLogger(SpringJenkinsApplication.class);
+	
+	@Test
+	public void contextLoads() {
+		logger.info("Test case executing..");
+		assertEquals(true,true);
+		
+	}
+
+}
